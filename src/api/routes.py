b8d95e3588dbc6baf6ea05e608a1520d941aa3b0
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2024-02-05 01:42:08 +0000

    second save

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,19 +4,17 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
+#create flask app
 api = Blueprint('api', __name__)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 
-# Allow CORS requests to this API
-CORS(api)
+@api.route('/token', methods=['POST'])
+def create_token():
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
+    # Get the request body data
     return jsonify(response_body), 200
